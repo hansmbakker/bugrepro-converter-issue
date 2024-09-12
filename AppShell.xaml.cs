b4697ae2f8723e68478a8932a501d8a823ef3024
Hans Bakker <hans.bakker@xebia.com>
2024-09-12 09:19:55 +0200

    first commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace bugrepro_converter_issue;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace bugrepro_converter_issue;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
